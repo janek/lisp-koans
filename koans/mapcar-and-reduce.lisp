b870ce2b44b2d623723439c21d71be9a0eaeccd2
Janek Szynal <jan.szynal@gmail.com>
2020-10-19 22:17:24 +0200

    117 koans, stuck on loops

--- koans/mapcar-and-reduce.lisp
+++ koans/mapcar-and-reduce.lisp
@@ -69,29 +69,29 @@
   ;; The function REDUCE combines the elements of a list by applying a binary
   ;; function to the elements of a sequence from left to right.
   (assert-equal 15 (reduce #'+ '(1 2 3 4 5)))
-  (assert-equal ____ (reduce #'+ '(1 2 3 4)))
-  (assert-equal ____ (reduce #'expt '(1 2 3 4 5))))
+  (assert-equal 10 (reduce #'+ '(1 2 3 4)))
+  (assert-equal 1 (reduce #'expt '(1 2 3 4 5))))
 
 (define-test reduce-from-end
   ;; The :FROM-END keyword argument can be used to reduce from right to left.
   (let ((numbers '(1 2 3 4 5)))
-    (assert-equal ____ (reduce #'cons numbers))
-    (assert-equal ____ (reduce #'cons numbers :from-end t)))
+    (assert-equal '((((1 . 2) . 3) . 4) . 5) (reduce #'cons numbers))
+    (assert-equal '(1 2 3 4 . 5) (reduce #'cons numbers :from-end t)))
   (let ((numbers '(2 3 2)))
-    (assert-equal ____ (reduce #'expt numbers))
-    (assert-equal ____ (reduce #'expt numbers :from-end t))))
+    (assert-equal 64 (reduce #'expt numbers))
+    (assert-equal 512 (reduce #'expt numbers :from-end t))))
 
 (define-test reduce-initial-value
   ;; :INITIAL-VALUE can supply the initial value for the reduction.
   (let ((numbers '(1 2 3 4 5)))
-    (assert-equal ____ (reduce #'* numbers))
-    (assert-equal ____ (reduce #'* numbers :initial-value 0))
-    (assert-equal ____ (reduce #'* numbers :initial-value -1))))
+    (assert-equal 120 (reduce #'* numbers))
+    (assert-equal 0 (reduce #'* numbers :initial-value 0))
+    (assert-equal -120 (reduce #'* numbers :initial-value -1))))
 
 (define-test inner-product
   ;; MAPCAR and REDUCE are powerful when used together.
   ;; Fill in the blanks to produce a local function that computes an inner
   ;; product of two vectors.
-  (flet ((inner-product (x y) (reduce ____ (mapcar ____ x y))))
+  (flet ((inner-product (x y) (reduce #'+ (mapcar #'* x y))))
     (assert-equal 32 (inner-product '(1 2 3) '(4 5 6)))
     (assert-equal 310 (inner-product '(10 20 30) '(4 3 7)))))
